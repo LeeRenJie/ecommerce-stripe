d69d9d3924d5cc7dd2d33dc56f7548e7f2225663
Lee Ren Jie <work.renjie@gmail.com>
2023-10-25 01:20:23 +0800

    feat: run lint and add schema for banner and product

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
 
-import blockContent from './schemas/blockContent'
-import category from './schemas/category'
-import post from './schemas/post'
-import author from './schemas/author'
+import banner from './schemas/banner';
+import product from './schemas/product';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
-}
+  types: [banner, product],
+};
+
+export default schema;
